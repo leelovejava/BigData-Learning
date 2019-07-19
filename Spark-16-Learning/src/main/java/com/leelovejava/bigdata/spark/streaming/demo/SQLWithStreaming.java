c51e64ad052078017ec3bae5e7e0ac886fb9200e
tianhao <tianhao@caimom.com>
2019-07-19 15:37:35 +0800

    修改jdk编译版本

--- Spark-16-Learning/src/main/java/com/leelovejava/bigdata/spark/streaming/demo/SQLWithStreaming.java
+++ Spark-16-Learning/src/main/java/com/leelovejava/bigdata/spark/streaming/demo/SQLWithStreaming.java
@@ -20,73 +20,72 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
 /**
  * 在spark steaming 中使用sparksql查询
- * 
- * 
- * @author root
  *
+ * @author root
  */
 public class SQLWithStreaming {
-	public static void main(String[] args) {
-		
-		SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("WindowOnStreaming");
-		JavaStreamingContext jsc = new JavaStreamingContext(conf,Durations.seconds(5));
-		jsc.checkpoint("./checkpoint");
-		JavaReceiverInputDStream<String> lines = jsc.socketTextStream("node1", 9999);
-		
-		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+    public static void main(String[] args) {
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("WindowOnStreaming");
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
+        jsc.checkpoint("./checkpoint");
+        JavaReceiverInputDStream<String> lines = jsc.socketTextStream("node1", 9999);
 
-			public Iterable<String> call(String lines) throws Exception {
-				
-				return Arrays.asList(lines.split(" "));
-			}
-		});
-		/**
-		 * 每分钟计算最近一小时的数据量
-		 */
-		JavaDStream<String> windowWords = words.window(Durations.minutes(60), Durations.seconds(15));
-		
-		windowWords.foreachRDD(new VoidFunction<JavaRDD<String>>() {
-			
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
 
-			public void call(JavaRDD<String> JavaRDD) throws Exception {
-				org.apache.spark.api.java.JavaRDD<Row> wordRowRDD = JavaRDD.map(new Function<String, Row>() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
 
-					/**
-					 * 
-					 */
-					private static final long serialVersionUID = 1L;
+            public Iterable<String> call(String lines) throws Exception {
 
-					public Row call(String word) throws Exception {
-						return RowFactory.create(word);
-					}
-				});
-				
-				SQLContext sqlContext = SQLContext.getOrCreate(JavaRDD.context());
-				/**
-				 * 创建schema
-				 */
-				List<StructField> fields = new ArrayList<StructField>();
-				fields.add(DataTypes.createStructField("word", DataTypes.StringType, true));
-				StructType createStructType = DataTypes.createStructType(fields);
-				DataFrame allwords= sqlContext.createDataFrame(wordRowRDD, createStructType);
+                return Arrays.asList(lines.split(" "));
+            }
+        });
+        /**
+         * 每分钟计算最近一小时的数据量
+         */
+        JavaDStream<String> windowWords = words.window(Durations.minutes(60), Durations.seconds(15));
+
+        windowWords.foreachRDD(new VoidFunction<JavaRDD<String>>() {
+
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
+
+            public void call(JavaRDD<String> JavaRDD) throws Exception {
+                org.apache.spark.api.java.JavaRDD<Row> wordRowRDD = JavaRDD.map(new Function<String, Row>() {
+
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
+
+                    public Row call(String word) throws Exception {
+                        return RowFactory.create(word);
+                    }
+                });
+
+                SQLContext sqlContext = SQLContext.getOrCreate(JavaRDD.context());
+                /**
+                 * 创建schema
+                 */
+                List<StructField> fields = new ArrayList<StructField>();
+                fields.add(DataTypes.createStructField("word", DataTypes.StringType, true));
+                StructType createStructType = DataTypes.createStructType(fields);
+                DataFrame allwords = sqlContext.createDataFrame(wordRowRDD, createStructType);
 //				allwords.show();//显示打印
-				/**
-				 * 注册成临时表
-				 */
-				allwords.registerTempTable("words");
-				DataFrame result = sqlContext.sql("select word ,count(*) rank from words group by word order by rank");
-				result.show(10000);
+                /**
+                 * 注册成临时表
+                 */
+                allwords.registerTempTable("words");
+                DataFrame result = sqlContext.sql("select word ,count(*) rank from words group by word order by rank");
+                result.show(10000);
 //				org.apache.spark.api.java.JavaRDD<Row> javaRDD2 = result.javaRDD();
 //				javaRDD2.foreach(new VoidFunction<Row>() {
 //
@@ -99,11 +98,11 @@ public class SQLWithStreaming {
 //						System.out.println("row-------"+row);
 //					}
 //				});
-			}
-		});
-		
-		jsc.start();
-		jsc.awaitTermination();
-		jsc.close();
-	}
+            }
+        });
+
+        jsc.start();
+        jsc.awaitTermination();
+        jsc.close();
+    }
 }
--- Spark-16-Learning/src/main/java/com/leelovejava/bigdata/spark/streaming/transformations/window/Operate_countByValueAndWindow.java
+++ Spark-16-Learning/src/main/java/com/leelovejava/bigdata/spark/streaming/transformations/window/Operate_countByValueAndWindow.java
@@ -18,44 +18,37 @@ import scala.Tuple2;
  * （K，V）格式的Dstream使用时，每个Key的Value值根据Key在滑动窗口出现的次数，返回（K，Long）格式的Dstream。
  *
  * @author root
- *
  */
 public class Operate_countByValueAndWindow {
-	public static void main(String[] args) {
-		SparkConf conf = new SparkConf().setMaster("local").setAppName("countByValueAndWindow");
-		JavaStreamingContext jsc = new JavaStreamingContext(conf,Durations.seconds(5));
-		jsc.checkpoint("checkpoint");
-		JavaDStream<String> textFileStream = jsc.textFileStream("data");
-		/**
-		 * 首先将textFileStream转换为tuple格式统计word字数
-		 */
-		JavaPairDStream<String, Integer> mapToPair = textFileStream.flatMap(new FlatMapFunction<String, String>() {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("countByValueAndWindow");
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
+        jsc.checkpoint("checkpoint");
+        JavaDStream<String> textFileStream = jsc.textFileStream("data");
+        /**
+         * 首先将textFileStream转换为tuple格式统计word字数
+         */
+        JavaPairDStream<String, Integer> mapToPair = textFileStream.flatMap(new FlatMapFunction<String, String>() {
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
 
-			public Iterable<String> call(String t) throws Exception {
-				return Arrays.asList(t.split(" "));
-			}
-		}).mapToPair(new PairFunction<String, String, Integer>() {
+            private static final long serialVersionUID = 1L;
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
+            public Iterable<String> call(String t) throws Exception {
+                return Arrays.asList(t.split(" "));
+            }
+        }).mapToPair(new PairFunction<String, String, Integer>() {
+            private static final long serialVersionUID = 1L;
 
-			public Tuple2<String, Integer> call(String t) throws Exception {
-				return new Tuple2<String, Integer>(t.trim(), 1);
-			}
-		});
-		
-		JavaPairDStream<Tuple2<String, Integer>, Long> countByValueAndWindow = mapToPair.countByValueAndWindow(Durations.seconds(15), Durations.seconds(5));
-		countByValueAndWindow.print();
-		
-		jsc.start();
-		jsc.awaitTermination();
-		jsc.close();
-	}
+            public Tuple2<String, Integer> call(String t) throws Exception {
+                return new Tuple2<String, Integer>(t.trim(), 1);
+            }
+        });
+
+        JavaPairDStream<Tuple2<String, Integer>, Long> countByValueAndWindow = mapToPair.countByValueAndWindow(Durations.seconds(15), Durations.seconds(5));
+        countByValueAndWindow.print();
+
+        jsc.start();
+        jsc.awaitTermination();
+        jsc.close();
+    }
 }
