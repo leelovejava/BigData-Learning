855a0f176dce5df4fbe85731b0fa032dfb0f54a0
leelovejava <1165746141@qq.com>
2019-09-15 16:50:07 +0800

    创建表指定字符集

--- Flume-Learning/flume-mysql-source/src/test/resources/create.sql
+++ Flume-Learning/flume-mysql-source/src/test/resources/create.sql
@@ -6,13 +6,13 @@ CREATE TABLE `student` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
  PRIMARY KEY (`id`)
-);
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='学生表';;
 
 CREATE TABLE `flume_meta` (
 `source_tab` varchar(255) NOT NULL,
 `currentIndex` varchar(255) NOT NULL,
  PRIMARY KEY (`source_tab`)
-);
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='flume元数据表';;
 
 insert into `student`(name) values ('zhangsan');
 insert into `student`(name) values ('lisi');
